25aa904dd93480648eb1c8c926b2f09fe0e586ba
keane <152969444+KeaneHh@users.noreply.github.com>
2025-06-20 10:19:57 +0800

    Bug fix/remove babylon (#67)
    * remove babylon sync
    
    * remove babylon sync
    
    * remove node babylon sync

--- node/node.go
+++ node/node.go
@@ -111,30 +111,30 @@ func NewFinalityNode(ctx context.Context, db *store.Storage, privKey *ecdsa.Priv
 	metricer := metrics.NewMetrics(registry)
 
 	txMsgChan := make(chan store.TxMessage, 100)
-	babylonSynchronizer, err := synchronizer.NewBabylonSynchronizer(ctx, cfg, db, shutdown, logger, txMsgChan, metricer)
-	if err != nil {
-		return nil, err
-	}
+	//babylonSynchronizer, err := synchronizer.NewBabylonSynchronizer(ctx, cfg, db, shutdown, logger, txMsgChan, metricer)
+	//if err != nil {
+	//	return nil, err
+	//}
 	celestiaSynchronizer, err := synchronizer.NewCelestiaSynchronizer(ctx, cfg, db, shutdown, logger, authToken, metricer)
 	if err != nil {
 		return nil, err
 	}
 
 	return &Node{
-		wg:                   sync.WaitGroup{},
-		done:                 make(chan struct{}),
-		stopChan:             make(chan struct{}),
-		log:                  logger,
-		db:                   db,
-		privateKey:           privKey,
-		from:                 from,
-		cfg:                  cfg,
-		ctx:                  ctx,
-		msmContract:          msmContract,
-		wsClient:             wsClient,
-		keyPairs:             keyPairs,
-		signRequestChan:      make(chan tdtypes.RPCRequest, 100),
-		babylonSynchronizer:  babylonSynchronizer,
+		wg:              sync.WaitGroup{},
+		done:            make(chan struct{}),
+		stopChan:        make(chan struct{}),
+		log:             logger,
+		db:              db,
+		privateKey:      privKey,
+		from:            from,
+		cfg:             cfg,
+		ctx:             ctx,
+		msmContract:     msmContract,
+		wsClient:        wsClient,
+		keyPairs:        keyPairs,
+		signRequestChan: make(chan tdtypes.RPCRequest, 100),
+		//babylonSynchronizer:  babylonSynchronizer,
 		celestiaSynchronizer: celestiaSynchronizer,
 		txMsgChan:            txMsgChan,
 		metricsRegistry:      registry,
@@ -162,9 +162,9 @@ func (n *Node) Start(ctx context.Context) error {
 	n.wg.Add(3)
 	go n.ProcessMessage()
 	go n.sign()
-	go n.work()
+	//go n.work()
 
-	go n.babylonSynchronizer.Start()
+	//go n.babylonSynchronizer.Start()
 	go n.celestiaSynchronizer.Start()
 
 	if err := n.startMetricsServer(); err != nil {
@@ -178,7 +178,7 @@ func (n *Node) Stop(ctx context.Context) error {
 	n.cancel()
 	close(n.done)
 	n.wg.Wait()
-	n.babylonSynchronizer.Close()
+	//n.babylonSynchronizer.Close()
 	n.celestiaSynchronizer.Close()
 	if n.metricsServer != nil {
 		if err := n.metricsServer.Close(); err != nil {
@@ -396,15 +396,15 @@ func (n *Node) SignMessage(requestBody types.SignMsgRequest) (*sign.Signature, e
 }
 
 func (n *Node) checkSyncStatus(end uint64) bool {
-	babylonSynced := uint64(n.babylonSynchronizer.HeaderTraversal.LastTraversedHeader().Time.Unix()) >= end
+	//babylonSynced := uint64(n.babylonSynchronizer.HeaderTraversal.LastTraversedHeader().Time.Unix()) >= end
 	celestiaSynced := uint64(n.celestiaSynchronizer.HeaderTraversal.LastTraversedHeader().Time().Unix()) >= end
 
-	if !babylonSynced {
-		n.log.Warn("Babylon sync not completed",
-			"required", end,
-			"current", n.babylonSynchronizer.HeaderTraversal.LastTraversedHeader().Time.Unix())
-		return false
-	}
+	//if !babylonSynced {
+	//	n.log.Warn("Babylon sync not completed",
+	//		"required", end,
+	//		"current", n.babylonSynchronizer.HeaderTraversal.LastTraversedHeader().Time.Unix())
+	//	return false
+	//}
 
 	if !celestiaSynced {
 		n.log.Warn("Celestia sync not completed",
